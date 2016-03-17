795d1d1acbedfb6ae3856d43883cf1a903eff814
Deathstroke7 <webhead561@gmail.com>
2016-03-17 15:19:58 +0530

    menpo updated

--- LipFeatures/lipFeatures.py
+++ LipFeatures/lipFeatures.py
@@ -7,6 +7,8 @@ import menpodetect
 from menpodetect.opencv import *
 import menpo.image as mimg
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from ImageCapture import *
 import AAMObject
 from constants import *
@@ -21,28 +23,23 @@ class lipFeatures:
 
     def getGrayscaleImage(self):
         img = getImage(0,pathToTestset+'/image_0003.png')
-        print type(img)
-        #gray = cv2.equalizeHist(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-        #image = mimg.Image(gray)
         return img
 
     def processImage(self, image, aam_fitter):
         detector = load_opencv_frontal_face_detector()
         detector(image)
-        result = aam_fitter.fit_from_bb(image,image.landmarks['PTS'].lms, max_iters=10)
-        # print type(result)
-        # img = result.final_shape
-        # img = result.iter_image
-        # st = "iter_" + str(result.n_iters)
-        # print img.landmarks[st]['n_points']
-        # print b.labels        
-        # print type(img)
-        # print img.landmarks['iter_0']
+        result = aam_fitter.fit_from_shape(image,image.landmarks['PTS'].lms, max_iters=10)
+        print type(result)
+        img = result.final_shape
+        image.view()
+        img.view()
+        plt.show()
+        print img
+        print img.points
 
 
 x = lipFeatures()
 y = x.getAAMFitter()
 
-while True:
-    z = x.getGrayscaleImage()
-    m = x.processImage(z, y)
+z = x.getGrayscaleImage()
+m = x.processImage(z, y)
